e227a214dae00deb35f78c90107475b25ae12145
Sayuri-Suganuma <sayuri.suganuma@o-g.co.jp>
2024-06-05 10:40:08 +0900

    cors.rbへの追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4200"
+    origins "http://localhost:4200", "https://dev-sugaapp-fe.sakuramobile.jp"
 
     resource "*",
       headers: :any,
